055af6f92db61fef51872ecc2c9e638ad16a36b4
gaexp <gaexp@baek>
2020-09-14 22:57:36 +0900

    sad

--- singleproject/WebContent/database/goods.sql
+++ singleproject/WebContent/database/goods.sql
@@ -23,5 +23,14 @@ price number(10),
 id varchar2(100)
 );
 
+create table history(
+his_no number primary key,
+limit timestamp,
+picture varchar2(100),
+id varchar2(100),
+no varchar2(100));
+
+select a.limit ,a.picture , b.id  ,a.no, max(b.price) from auction a , bidding b where a.no = b.no and systimestamp > a.limit group by b.id,a.limit ,a.picture ,a.no;
+from auction a where systimestamp > limit;
 bidding 을 옥션 만듦과 동시에 생성 
 추가 입찰건에 관해서는 업데이트 적용
---
+++ singleproject/src/model/HistoryVO.java
@@ -0,0 +1,57 @@
+package model;
+
+public class HistoryVO {
+	private int his_no;
+	private String limit;
+	private String picture;
+	private String id;
+	private int no;
+	public int getHis_no() {
+		return his_no;
+	}
+	public void setHis_no(int his_no) {
+		this.his_no = his_no;
+	}
+	public String getLimit() {
+		return limit;
+	}
+	public void setLimit(String limit) {
+		this.limit = limit;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public HistoryVO(int his_no, String limit, String picture, String id, int no) {
+		super();
+		this.his_no = his_no;
+		this.limit = limit;
+		this.picture = picture;
+		this.id = id;
+		this.no = no;
+	}
+	public HistoryVO() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "HistoryVO [his_no=" + his_no + ", limit=" + limit + ", picture=" + picture + ", id=" + id + ", no=" + no
+				+ "]";
+	}
+	
+	
+}
